74f7bca3c2481a9f87be1463516e881631a6248a
storonaot <storonaot@gmail.com>
2016-07-24 23:28:58 +0300

    added files

---
+++ frontend/javascripts/tasks.js
@@ -0,0 +1,173 @@
+Vue.component('my-tasks', {
+  template: '#tasks',
+  data() {
+    return {
+      task: null,
+      rate: null,
+      running: false,
+      seconds: '00',
+      minutes: '00',
+      hours: '00',
+      time: null,
+      tasks: [],
+      activeTask: null,
+      spentTime: null,
+      earnedMoney: null,
+      activeColor: 'gray',
+    }
+  },
+  ready() {
+    this.getLocalStorage()
+    this.calcTotals()
+    this.allToInactive()
+  },
+  methods: {
+    timeCounter() {
+      this.seconds < 9 ? this.seconds = '0' + ++this.seconds : this.seconds++
+      if (this.seconds >= 60) {
+        this.seconds = '00'
+        this.minutes < 9 ? this.minutes = '0' + ++this.minutes : this.minutes++
+        if (this.minutes >= 60) {
+          this.minutes = '00'
+          this.hours < 9 ? this.hours = '0' + ++this.hours : this.hours++
+        }
+      }
+      this.startTimer()
+    },
+    startTimer() {
+      this.running = true
+      this.time = setTimeout(this.timeCounter, 1000)
+    },
+    stopTimer() {
+      this.running = false
+      clearTimeout(this.time)
+      this.addTask()
+      this.task = null
+      this.rate = null
+      this.seconds = '00'
+      this.minutes = '00'
+      this.hours = '00'
+      this.calcTotals()
+      this.setLocalStorage()
+      this.allToInactive()
+    },
+    addTask() {
+      let time = this.hours + ':' + this.minutes + ':' + this.seconds
+      let total = this.calcPrice()
+      let totalSeconds = +this.hours * 3600 + +this.minutes * 60 + +this.seconds
+
+      if (!this.task) {
+        this.task = '(не заполнено)'
+      }
+
+      this.tasks.unshift(
+        { name: this.task, rate: this.rate, time: time, total: total, totalSeconds: totalSeconds, flag: false }
+      )
+    },
+    roundingToHours() {
+      return Math.ceil((this.hours * 3600 + this.minutes * 60 + this.seconds)/3600)
+    },
+    calcPrice() {
+      this.rate = parseInt(this.rate)
+      if (isNaN(this.rate)) {
+        this.rate = 0
+      }
+
+      return this.roundingToHours() * this.rate
+    },
+    editTask(index) {
+      this.tasks[index].flag = true
+      this.newName = null
+      this.getActiveTask(index)
+      this.calcTotals()
+      this.setLocalStorage()
+    },
+    deleteTask(index) {
+      this.tasks.splice(index, 1)
+      this.calcTotals()
+      this.setLocalStorage()
+    },
+    saveEditedTask(index) {
+      let thisTask = this.tasks[index]
+      let roundToHour = Math.ceil(thisTask.totalSeconds / 3600)
+
+      if (!thisTask.name) {
+        thisTask.name = '(не заполнено)'
+      }
+
+      thisTask.total = roundToHour * thisTask.rate
+      thisTask.flag = false
+      this.calcTotals()
+      this.setLocalStorage()
+
+    },
+    calcTotals() {
+      let totalSeconds = null
+      let totalMoney = null
+      let seconds = null
+      let minutes = null
+      let hours = null
+      let reduce = null
+
+      this.tasks.forEach(task => {
+        totalSeconds += task.totalSeconds
+        totalMoney += task.total
+      })
+
+      reduce = totalSeconds % 3600
+      hours = (totalSeconds - reduce)/3600
+      totalSeconds = reduce
+      reduce = totalSeconds % 60
+      minutes = (totalSeconds - reduce)/60
+      seconds = reduce
+      hours = hours < 10 ? '0' + hours : hours
+      minutes = minutes < 10 ? '0' + minutes : minutes
+      seconds = seconds < 10 ? '0' + seconds : seconds
+
+      this.spentTime = hours + ':' + minutes + ':' + seconds
+      this.earnedMoney = totalMoney
+    },
+    getActiveTask(index) {
+      if (this.activeTask !== null) {
+        let thisTask = this.tasks[this.activeTask]
+        let roundToHour = Math.ceil(thisTask.totalSeconds / 3600)
+        thisTask.flag = false
+        thisTask.total = roundToHour * thisTask.rate
+
+        if (!thisTask.name) {
+          thisTask.name = '(не заполнено)'
+        }
+
+        thisTask.total = roundToHour * thisTask.rate
+        thisTask.flag = false
+        this.calcTotals()
+        this.setLocalStorage()
+      }
+      if (this.activeTask === index) {
+        this.tasks[this.activeTask].flag = true
+      }
+      this.activeTask = index
+    },
+    setLocalStorage() {
+      localStorage.setItem('tasks', JSON.stringify(this.tasks))
+      localStorage.setItem('activeColor', JSON.stringify(this.activeColor))
+    },
+    getLocalStorage() {
+      this.tasks = JSON.parse(localStorage.getItem('tasks'))
+      this.activeColor = JSON.parse(localStorage.getItem('activeColor'))
+    },
+    allToInactive() {
+      this.tasks.forEach(task => {
+        task.flag = false
+      })
+    },
+    getColor(color) {
+      this.activeColor = color
+      this.setLocalStorage()
+    },
+  },
+})
+
+new Vue({
+  el: '#app',
+})
---
+++ gulpfile.js
@@ -0,0 +1,91 @@
+const gulp = require('gulp')
+
+const sass = require('gulp-sass')
+const autoprefixer = require('gulp-autoprefixer')
+const sourcemaps = require('gulp-sourcemaps')
+const del = require('del')
+const debug = require('gulp-debug')
+const browserSync = require('browser-sync').create()
+const jade = require('gulp-jade')
+const babel = require('gulp-babel')
+const concat = require('gulp-concat')
+const imagemin = require('gulp-imagemin')
+const rename = require('gulp-rename')
+
+const postcss = require('gulp-postcss')
+const inlineSVG = require('postcss-inline-svg')
+const processors = [inlineSVG()]
+
+gulp.task('styles:build', () => {
+  return gulp.src('frontend/styles/main.sass', {base: 'frontend'})
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(debug({title: 'styles'}))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(postcss(processors))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('public'))
+})
+
+gulp.task('html:build', () => {
+  return gulp.src('frontend/**/*.jade')
+    .pipe(debug({title: 'html'}))
+    .pipe(jade())
+    .pipe(gulp.dest('public'))
+})
+
+gulp.task('js:build', () => {
+  return gulp.src('frontend/javascripts/**/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(debug({title: 'js'}))
+    .pipe(babel())
+    .pipe(debug({title: 'js'}))
+    .pipe(concat('main.js'))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('public/javascripts'));
+})
+
+gulp.task('images:build', () => {
+  return gulp.src('frontend/images/**/*.*', {base: 'frontend'})
+    .pipe(imagemin())
+    .pipe(gulp.dest('public'));
+})
+
+gulp.task('fonts:build', () => {
+  return gulp.src('frontend/fonts/**/*.*', {base: 'frontend'})
+  .pipe(gulp.dest('public'));
+})
+
+gulp.task('clean', () => {
+  return del('public')
+})
+
+gulp.task('build',
+  gulp.series('clean',
+    gulp.parallel('js:build', 'html:build', 'styles:build', 'images:build', 'fonts:build')
+  )
+)
+
+gulp.task('watch', () => {
+  gulp.watch('frontend/styles/**/*.sass', gulp.series('styles:build'))
+  gulp.watch('frontend/javascripts/*.js', gulp.series('js:build'))
+  gulp.watch('frontend/**/*.jade', gulp.series('html:build'))
+  gulp.watch('frontend/images/**/*.*', gulp.series('images:build'))
+  gulp.watch('frontend/fonts/**/*.*', gulp.series('fonts:build'))
+})
+
+gulp.task('serve', () => {
+  browserSync.init({
+    server: 'public',
+    port: 8080,
+    ui: {
+      port: 8081,
+    },
+  })
+  browserSync.watch('public/**/*.*').on('change', browserSync.reload)
+})
+
+gulp.task('dev', gulp.series('build', gulp.parallel('watch', 'serve')))
